a5eb2154694952eca1c3c564b6513f9eaae776eb
anettera <27014495+remove32@users.noreply.github.com>
2025-06-03 10:03:56 +0100

    Update publish_multi_request.py
    Signed-off-by: anettera <27014495+remove32@users.noreply.github.com>

--- Tools/publish_multi_request.py
+++ Tools/publish_multi_request.py
@@ -14,8 +14,8 @@ RELEASE_DIR = "release"
 # CONFIGURATION PARAMETERS
 # Forks should change these to publish to their own infrastructure.
 #
-ROBUST_CDN_URL = "https://cdn.simplestation.org/"
-FORK_ID = "einstein-engines"
+ROBUST_CDN_URL = "https://cdn.fulp.gg/"
+FORK_ID = "einstein"
 
 def main():
     session = requests.Session()
